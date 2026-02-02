83ce89301b9c932f77c7790b8d99f740362667dc
Павел Дорофеев <dorofeevpavelwow@gmail.com>
2026-02-02 14:58:11 +0300

    1.4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,5 +118,7 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://frontend:3000',
 ]
